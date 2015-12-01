bacb91b53a45085f739801bbb2709399ef3c8524
root <root@lab01.(none)>
2015-12-01 17:45:07 +0800

    recommendation on tvcinema

--- tvcinema/CinemaContentRec.scala
+++ tvcinema/CinemaContentRec.scala
@@ -3,7 +3,7 @@ package com.ctvit.tvcinema
 import java.sql.{ResultSet, DriverManager}
 import java.text.SimpleDateFormat
 import java.util
-import java.util.Date
+import java.util.{Random, Date}
 
 import com.ctvit.{MysqlFlag, AllConfigs}
 import net.sf.json.JSONObject
@@ -11,6 +11,9 @@ import org.apache.spark.rdd.JdbcRDD
 import org.apache.spark.{SparkContext, SparkConf}
 import redis.clients.jedis.Jedis
 import scopt.OptionParser
+
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * Created by BaiLu on 2015/11/24.
@@ -31,11 +34,14 @@ object CinemaContentRec {
 
   val MYSQL_CONNECT = "jdbc:mysql://" + MYSQL_HOST + ":" + MYSQL_PORT + "/" + MYSQL_DB
   val MYSQL_DRIVER = "com.mysql.jdbc.Driver"
-
+  val MYSQL_QUERY = "select id,sort_index from catalog_info where parent_id ='10001719' and sort_index is not null;"
+  val MYSQL_CONTENTNAME_QUERY = "select a.id,a.name from (select id,name,parent_id from element_info)a join (select id from catalog_info where parent_id='10001648')b on a.parent_id=b.id;"
 
   val REDIS_IP = configs.BOX_REDIS_IP
   val REDIS_IP2 = configs.BOX_REDIS_IP2
   val REDIS_PORT = configs.BOX_REDIS_PORT
+
+  val random = new Random()
 
   def main(args: Array[String]) {
     val startTime = System.nanoTime()
@@ -76,17 +82,28 @@ object CinemaContentRec {
 
     val conf = new SparkConf().setAppName("ContentRecNew")
     val sc = new SparkContext(conf)
+    val cidname = getCidName(MYSQL_CONTENTNAME_QUERY)
     val tvcinema_rdd = new JdbcRDD(sc, initMySQL, "select id,sort_index from catalog_info where parent_id ='10001719' and sort_index is not null and id>=? and id <= ?;", 1, 2000000000, 10, extractValues)
       .map { tup => mapEveryContentid(tup._1, tup._2)}
       .flatMap { line => val field = line.split("#")
       field
     }
       .map { tup => val field = tup.split(","); (field(0), field(1))}
+      .map(tup => (tup._1, (tup._2, cidname.getOrElse(tup._2, " "))))
+
     val tvcinema = tvcinema_rdd.join(tvcinema_rdd)
-      .filter(tup => tup._2._1 != tup._2._2)
-      .map(tup => (tup._2._1, tup._2._2))
+      .filter(tup => tup._2._1._1 != tup._2._2._1)
+      .filter(tup => tup._2._1._2 != tup._2._2._2)
+      //targetid,recontentid
+      //      .map(tup => (tup._2._1._1, tup._2._2._1))
+      //      .groupByKey(15)
+      //      .foreach(tup => insertRedis(tup._1, tup._2, params.recNumber))
+
+      //targetid,(recontentid,recontentname)
+      .map(tup => (tup._2._1._1, (tup._2._2._1, tup._2._2._2)))
       .groupByKey(15)
-      .foreach(tup=>insertRedis(tup._1,tup._2,params.recNumber))
+      .map(tup=>(tup._1,mapCidName(tup._2)))
+      .foreach(tup => insertRedis(tup._1, tup._2, params.recNumber))
 
     sc.stop()
   }
@@ -121,8 +138,69 @@ object CinemaContentRec {
     mapStr
   }
 
+  def mapCidName(iterable: Iterable[(String, String)]): String = {
+    val list = iterable.toList
+    val listLen = list.length
+    var i = 0
+    var recStr = ""
+    while (i<listLen){
+      recStr+=list(i)._1+","+list(i)._2+"#"
+      i+=1
+    }
+    recStr
+  }
 
-  def insertRedis(targetContent: String, iterable: Iterable[(String)], recnumber: Int): Unit = {
+  def getCidName(sql: String): mutable.HashMap[String, String] = {
+    val init = initMySQL()
+    val rs = init.createStatement().executeQuery(sql)
+    val map = new mutable.HashMap[String, String]()
+    while (rs.next()) {
+      val contentid = rs.getString(1)
+      val contentname = rs.getString(2)
+      map.put(contentid, contentname)
+    }
+    map
+  }
+
+  def getCids(sql: String): mutable.ArrayBuffer[String] = {
+    val init = initMySQL()
+    val rs = init.createStatement().executeQuery(sql)
+    val arraybuffer = new ArrayBuffer[String]()
+    while (rs.next()) {
+      val sortindex = rs.getString(2)
+      val arr = sortindex.split(";")
+      for (i <- 0 until arr.length) {
+        arraybuffer.append(arr(i))
+      }
+    }
+    init.close()
+    arraybuffer
+  }
+
+  def getNoRepeate(n: Int): Array[Int] = {
+
+    val sequence = new Array[Int](n)
+    val output = new Array[Int](n)
+    for (i <- 0 until n) {
+      sequence(i) = i
+    }
+    var end = n - 1
+    var i = 0
+    while (i < n) {
+      val index = random.nextInt(end + 1)
+      output(i) = sequence(index)
+      sequence(index) = sequence(end)
+      end -= 1
+      i += 1
+    }
+    output
+  }
+
+  val contentlist = getCids(MYSQL_QUERY)
+
+  //补齐方法，同一个栏目下的结果会出现相同的
+  //需要补齐
+  def insertRedis(targetContent: String, iterable:String, recnumber: Int): Unit = {
     val jedis = initRedis(REDIS_IP, REDIS_PORT)
     val jedis2 = initRedis(REDIS_IP2, REDIS_PORT)
 
@@ -131,30 +209,76 @@ object CinemaContentRec {
 
     val map = new util.HashMap[String, String]()
     val key = targetContent + "_5_10001719_0"
-    val arr = iterable.toList
+    val arr = iterable.split("#")
     val keynum = jedis.llen(key).toInt
     val keynum2 = jedis2.llen(key).toInt
-    if (arr.length > 2) {
-      var i = 0
-      while (i < arr.length && i < recnumber) {
-        val recAssetId = ""
-        val recAssetName = ""
-        val recAssetPic = ""
-        val recContentId = arr(i)
-        val recProviderId = ""
-        val rank = (i + 1).toString
-        map.put("assetId", recAssetId)
-        map.put("assetname", recAssetName)
-        map.put("assetpic", recAssetPic)
-        map.put("movieID", recContentId)
-        map.put("providerId", recProviderId)
-        map.put("rank", rank)
-        val value = JSONObject.fromObject(map).toString
 
-        pipeline.rpush(key, value)
-        pipeline2.rpush(key, value)
-        i += 1
+    if (arr.length > 2) {
+      if (arr.length >= 20) {
+        var i = 0
+        val indexArr = getNoRepeate(recnumber)
+        while (i < indexArr.length) {
+          val recAssetId = ""
+          val recAssetName = arr(indexArr(i)).split(",")(1)
+          val recAssetPic = ""
+          val recContentId = arr(indexArr(i)).split(",")(0)
+          val recProviderId = ""
+          val rank = (i + 1).toString
+          map.put("assetId", recAssetId)
+          map.put("assetname", recAssetName)
+          map.put("assetpic", recAssetPic)
+          map.put("movieID", recContentId)
+          map.put("providerId", recProviderId)
+          map.put("rank", rank)
+          val value = JSONObject.fromObject(map).toString
+          pipeline.rpush(key, value)
+          pipeline2.rpush(key, value)
+          i += 1
+        }
       }
+      else {
+        var j = 0
+        while (j < arr.length) {
+          val recAssetId = ""
+          val recAssetName = arr(j).split(",")(1)
+          val recAssetPic = ""
+          val recContentId = arr(j).split(",")(0)
+          val recProviderId = ""
+          val rank = (j + 1).toString
+          map.put("assetId", recAssetId)
+          map.put("assetname", recAssetName)
+          map.put("assetpic", recAssetPic)
+          map.put("movieID", recContentId)
+          map.put("providerId", recProviderId)
+          map.put("rank", rank)
+          val value = JSONObject.fromObject(map).toString
+          pipeline.rpush(key, value)
+          pipeline2.rpush(key, value)
+          j += 1
+        }
+        //不足二十个补齐
+        var k = 0
+        val indexArr = getNoRepeate(contentlist.length)
+        while (k < recnumber - arr.length) {
+          val recAssetId = ""
+          val recAssetName = ""
+          val recAssetPic = ""
+          val recContentId = contentlist(indexArr(k))
+          val recProviderId = ""
+          val rank = (k + arr.length + 1).toString
+          map.put("assetId", recAssetId)
+          map.put("assetname", recAssetName)
+          map.put("assetpic", recAssetPic)
+          map.put("movieID", recContentId)
+          map.put("providerId", recProviderId)
+          map.put("rank", rank)
+          val value = JSONObject.fromObject(map).toString
+          pipeline.rpush(key, value)
+          pipeline2.rpush(key, value)
+          k += 1
+        }
+      }
+
       for (j <- 0 until keynum) {
         pipeline.lpop(key)
       }
@@ -163,7 +287,6 @@ object CinemaContentRec {
         pipeline2.lpop(key)
       }
       pipeline2.sync()
-
     }
     jedis.disconnect()
     jedis2.disconnect()
